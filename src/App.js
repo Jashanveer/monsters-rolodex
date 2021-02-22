e04576bae9381108f9ac27257b3dd8f2c192f522
Jashanveer Singh <mailforjashanveer@gmail.com>
2021-02-22 16:54:31 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
+class App extends Component{
+  constructor(){
+    super();   // calls the constructor to the components class
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    this.state = {
+      monsters: [],
+      searchField : ''
+    };
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value })
+  }
+
+
+    render(){
+
+      const { monsters, searchField } = this.state; // destructuring
+      // const monsters = this.state.monsters; 
+      // const searchField = this.state.searchField 
+      const filteredMonsters = monsters.filter(monster => 
+          monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+
+        return (
+
+          <div className="App">
+            <h1>Monster Rolodex</h1>
+            <SearchBox
+            placeholder = 'search monsters'
+            handleChange={this.handleChange}
+          ></SearchBox>
+            <CardList monsters = {filteredMonsters}></CardList>
+          </div>
+        )
+    }
+  }
 
 export default App;
