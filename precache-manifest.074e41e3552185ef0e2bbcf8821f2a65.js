29b75a41adbf6f3abc0caddb5ea2e3b0dfd3f118
Jashanveer Singh <mailforjashanveer@gmail.com>
2021-02-22 16:53:21 +0530

    Updates

---
+++ precache-manifest.074e41e3552185ef0e2bbcf8821f2a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f559b21235b3f3c9127a8bce8a012768",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bbf60357a1c9423de82f",
+    "url": "/monsters-rolodex/static/css/main.3b2e47d1.chunk.css"
+  },
+  {
+    "revision": "eeb5311876b9e1bc2c36",
+    "url": "/monsters-rolodex/static/js/2.4e16f5fe.chunk.js"
+  },
+  {
+    "revision": "b333447af8aaab9ae624",
+    "url": "/monsters-rolodex/static/js/3.053f376b.chunk.js"
+  },
+  {
+    "revision": "bbf60357a1c9423de82f",
+    "url": "/monsters-rolodex/static/js/main.de5ff1e5.chunk.js"
+  },
+  {
+    "revision": "5393f8b62ce34e5ea4e5",
+    "url": "/monsters-rolodex/static/js/runtime-main.d8dc0fa9.js"
+  }
+]);
